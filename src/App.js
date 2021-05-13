5dd44bdc136ed1eb8945a3aad737ae825aebbfa2
slav4ocom <slav4o_1@abv.bg>
2021-05-14 02:27:46 +0300

    .

--- src/App.js
+++ src/App.js
@@ -80,6 +80,17 @@ class Page extends React.Component {
     }
 }
 
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  }
+  
 class Home extends React.Component {
     render() {
         return (
