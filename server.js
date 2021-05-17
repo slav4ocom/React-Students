b9ed55ae1294d223217d0c63218305ec3c36763d
slav4ocom <slav4o_1@abv.bg>
2021-05-18 01:29:46 +0300

    .

--- server.js
+++ server.js
@@ -57,48 +57,6 @@ app.get('/*/*', function (req, res) {
 });
 
 
-/*app.get('/include/*', function (req, res) {
-    var fileName = req.url;
-    var fileExt = fileName.split(".")[1];
-
-    //console.log(fileExt);
-
-    fs.readFile("./" + req.url, function (err, data) {
-        if (err) {
-            console.log(err);
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write(req.url + "  not found on this server<br/><br/>");
-            res.write("------------------------------------------- <br/>");
-            res.write("<i>slav4o.com web server at port " + port);
-            res.end();
-        }
-        else {
-            var contentType = "";
-            switch (fileExt) {
-                case "htm":
-                case "html":
-                    contentType = "text/html";
-                    break;
-
-                case "css":
-                    contentType = "text/css";
-                    break;
-
-                default:
-                    contentType = "text/plain";
-            }
-
-            var contentTypeString = { "Content-Type": contentType };
-            res.writeHead(200, contentTypeString);
-            res.write(data);
-            res.end();
-        }
-    });
-
-
-});*/
-
-
 app.get('/*', function (req, res) {
     var fileName = req.url;
     var fileExt = fileName.split(".")[1];
